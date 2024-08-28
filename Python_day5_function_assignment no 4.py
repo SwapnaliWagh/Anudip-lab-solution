b4bfc7d803cd69607355aaa56c9c7eb0e8017e38
SwapnaliWagh <102720948+SwapnaliWagh@users.noreply.github.com>
2024-08-28 20:06:21 +0530

    Add files via upload

---
+++ Python_day5_function_assignment no 4.py
@@ -0,0 +1,12 @@
+#4.Accept a name from the user and display that in lower case using lower() function.
+
+# Accept the name from the user
+name = input("Enter your name: ")
+
+# Convert the name to lowercase and display it
+lowercase_name = name.lower()
+print(f"Your name in lowercase is: {lowercase_name}")
+
+
+"""Ans=>Enter your name: HOSPITAL
+Your name in lowercase is: hospital"""
