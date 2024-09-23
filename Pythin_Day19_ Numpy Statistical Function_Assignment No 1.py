b19ae630124e6d6b932d4eca3ad2ed73bfef4861
SwapnaliWagh <102720948+SwapnaliWagh@users.noreply.github.com>
2024-09-23 14:57:24 +0530

    Add files via upload

---
+++ Pythin_Day19_ Numpy Statistical Function_Assignment No 1.py
@@ -0,0 +1,16 @@
+#Q.11.Compute the median of the flattened NumPy array Input: x_odd = np.array([1, 2, 3, 4, 5, 6, 7])
+
+import numpy as np  #Import the NumPy library
+
+#Input array
+x_odd = np.array([1, 2, 3, 4, 5, 6, 7])
+
+#Compute the median of the array
+median_value = np.median(x_odd)
+
+#Output the result
+print("Median of the given array is :", median_value)
+
+"""ANS=>> Median of the given array is : 4.0
+
+"""
